bb998743b799daa3dc8494f013d943292101f562
Yumana <yumanuralfath2@gmail.com>
2023-12-15 19:58:00 +0700

    v.0.8

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,7 +1,22 @@
+//Navbar Fixed
+
+window.onscroll = function () {
+  const header = document.querySelector('header');
+  const FixedNav = header.offsetTop;
+
+  if (window.scrollY > FixedNav) {
+    header.classList.add('navbar-fixed');
+  } else {
+    header.classList.remove('navbar-fixed');
+  }
+};
+
 // hamburger
 
 const hamburger = document.querySelector('#hamburger');
+const navMenu = document.querySelector('#nav-menu');
 
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('hamburger-active');
+  navMenu.classList.toggle('hidden');
 });
